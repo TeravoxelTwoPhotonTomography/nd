11d22a480a72c7f2812555c1a3c08f57199c5589
Nathan Clack <clackn@janelia.hhmi.org>
2012-10-02 11:27:18 -0400

    Problems with cuda config test program on mac...rpath

--- cmake/CheckCudaComputeCaps.cmake
+++ cmake/CheckCudaComputeCaps.cmake
@@ -7,15 +7,18 @@ if(CUDA_FOUND)
     ${CMAKE_CURRENT_SOURCE_DIR}/cmake/cuda_compute_capability.c
     CMAKE_FLAGS 
     -DINCLUDE_DIRECTORIES:STRING=${CUDA_TOOLKIT_INCLUDE}
-    -DLINK_LIBRARIES:STRING=${CUDA_CUDART_LIBRARY}
+    -DLINK_LIBRARIES:STRING=${CUDA_LIBRARIES}
     COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT_VAR
     RUN_OUTPUT_VARIABLE RUN_OUTPUT_VAR)
   if(COMPILE_RESULT_VAR AND NOT RUN_RESULT_VAR)
-    set(CUDA_HAVE_GPU TRUE CACHE BOOL "Whether CUDA-capable GPU is present")
+    #set(CUDA_HAVE_GPU TRUE CACHE BOOL "Whether CUDA-capable GPU is present")
     set(CUDA_COMPUTE_CAPABILITY ${RUN_OUTPUT_VAR} CACHE STRING "Compute capability of CUDA-capable GPU present")
     set(CUDA_GENERATE_CODE "arch=compute_${CUDA_COMPUTE_CAPABILITY},code=sm_${CUDA_COMPUTE_CAPABILITY}" CACHE STRING "Which GPU architectures to generate code for (each arch/code pair will be passed as --generate-code option to nvcc, separate multiple pairs by ;)")
     mark_as_advanced(CUDA_COMPUTE_CAPABILITY CUDA_GENERATE_CODE)
   else()
-    set(CUDA_HAVE_GPU FALSE CACHE BOOL "Whether CUDA-capable GPU is present")
+    ### TRY_RUN didn't work...guess?...I have library loading problems on osx...
+    set(CUDA_COMPUTE_CAPABILITY "30" CACHE STRING "Compute capability of CUDA-capable GPU present")
+    set(CUDA_GENERATE_CODE "arch=compute_${CUDA_COMPUTE_CAPABILITY},code=sm_${CUDA_COMPUTE_CAPABILITY}" CACHE STRING "Which GPU architectures to generate code for (each arch/code pair will be passed as --generate-code option to nvcc, separate multiple pairs by ;)")
+    #set(CUDA_HAVE_GPU FALSE CACHE BOOL "Whether CUDA-capable GPU is present")
   endif()
 endif(CUDA_FOUND)
